bc0e698a3c95e138648f878f7cdfa17720efa805
jonnyblair14 <51271024+jonnyblair14@users.noreply.github.com>
2024-05-25 18:24:49 +0100

    added outputfile param + handling for csv input and output with extra information about endpoint

--- MultiPing.ps1
+++ MultiPing.ps1
@@ -1,12 +1,38 @@
+param(
+  [string]$InputFile,
+  [string]$OutputFile
+)
 
-param($InputFile)
-$machines = Get-content $InputFile
+#Create a working array to store results
+$results = @()
 
-foreach ($machine in $machines){
- if (Test-Connection -ComputerName $machine -Count 1 -ErrorAction SilentlyContinue){
-   Write-Host "$machine,up" -ForegroundColor Green
- }
- else{
-   Write-Host "$machine,down" -ForegroundColor Red
-     } 
- }
+# Read the list of machines from the input .csv file
+$machines = Import-Csv $InputFile
+
+foreach ($machine in $machines) {
+  $ip = $machine.ipAddress
+  $status = if (Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue) {
+    "up"
+  } else {
+    "down"
+  }
+  
+  # Create custom object for each machine and add it to the results array
+  $resultObject = [PSCustomObject]@{
+    HOSTNAME = $machine.hostname
+    IP = $ip
+    MAC = $machine.macAddress
+    DESCRIPTION = $machine.description
+    STATUS = $status
+  }
+  $results += $resultObject
+
+  # Display result in the terminal
+  Write-Host "$($machine.hostname), $ip, $($machine.macAddress), $status" -ForegroundColor $(if ($status -eq 'up') {'Green'} else {'Red'})
+}
+
+# Export Results to .csv with headers
+$results | Export-Csv -Path $OutputFile -NoTypeInformation -Force
+
+# Display results in terminal
+$results
