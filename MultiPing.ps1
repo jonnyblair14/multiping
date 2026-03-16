5c3a2acb4fae47e34e23c49191d0b17d5d67b5bb
jonnyblair14 <51271024+jonnyblair14@users.noreply.github.com>
2026-03-16 13:41:44 +0000

    Improve MultiPing.ps1 output and add pause
    Updated output messages for online/offline status and added a pause at the end.

--- MultiPing.ps1
+++ MultiPing.ps1
@@ -3,10 +3,12 @@ param($InputFile)
 $machines = Get-content $InputFile
 
 foreach ($machine in $machines){
- if (Test-Connection -ComputerName $machine -Count 1 -ErrorAction SilentlyContinue){
-   Write-Host "$machine,up" -ForegroundColor Green
+ if (Test-Connection -ComputerName $machine -Count 1 -Quiet -ErrorAction SilentlyContinue){
+   Write-Host "$machine is online" -ForegroundColor Green
  }
  else{
-   Write-Host "$machine,down" -ForegroundColor Red
+   Write-Host "$machine is offline" -ForegroundColor Red
      } 
  }
+
+ Pause
